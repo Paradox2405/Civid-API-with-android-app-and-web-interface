7a76fc0bde32a52ad861e3810378313b74528854
Akila Geethanjana <akilageethanjana97@gmail.com>
2021-04-22 23:15:16 +0530

    creat qr scaner

--- app/src/main/java/com/thils/librarybook/qrscan.java
+++ app/src/main/java/com/thils/librarybook/qrscan.java
@@ -2,7 +2,11 @@ package com.thils.librarybook;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -13,12 +17,22 @@ import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.zxing.Result;
 
 public class qrscan extends AppCompatActivity {
+
     private CodeScanner mCodeScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(qrscan.this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(qrscan.this, new String[] {Manifest.permission.CAMERA}, 123);
+        } else {
+            startScanning();
+        }
+    }
+
+    private void startScanning() {
         CodeScannerView scannerView = findViewById(R.id.scanner_view);
         mCodeScanner = new CodeScanner(this, scannerView);
         mCodeScanner.setDecodeCallback(new DecodeCallback() {
@@ -41,14 +55,31 @@ public class qrscan extends AppCompatActivity {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 123) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_LONG).show();
+                startScanning();
+            } else {
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
-        mCodeScanner.startPreview();
+        if(mCodeScanner != null) {
+            mCodeScanner.startPreview();
+        }
     }
 
     @Override
     protected void onPause() {
-        mCodeScanner.releaseResources();
+        if(mCodeScanner != null) {
+            mCodeScanner.releaseResources();
+        }
         super.onPause();
     }
 }
